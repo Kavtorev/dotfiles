2ab82424475a9ba811c198b54976314c18d4ccbc
Kavtorev <57346893+Kavtorev@users.noreply.github.com>
2024-01-06 18:13:25 +0100

    improved load gnome settings

--- system/load_gnome_settings.sh
+++ system/load_gnome_settings.sh
@@ -2,18 +2,16 @@ sudo apt install -y gnome-tweak-tool && \
 sudo apt install -y sassc && \
 
 
-downloads_dir=~/Downloads
+downloads_dir="$HOME/Downloads"
 clone_dir="$downloads_dir/dash-to-dock"
 
 if [ -d "$clone_dir" ]; then
     echo "The 'dash-to-dock' folder already exists. Skipping cloning."
 else
-    cd "$downloads_dir" &&    
     git clone https://github.com/micheleg/dash-to-dock.git "$clone_dir" || exit
 fi
 
-cd "$clone_dir" || exit
-make install &&
+make -C "$clone_dir" install && \
 echo "Dash to Dock installation completed."
 
 
